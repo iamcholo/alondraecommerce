6e1af54adc20878731b9734e289ddedadab0be7e
xD <cocoaremix@gmail.com>
2018-02-12 14:16:43 -0800

    updating all data

--- website/src/payments/urls.py
+++ website/src/payments/urls.py
@@ -1,5 +1,5 @@
 from django.conf.urls import include, url
-from payment import rest_api as views 
+from payments import rest_api as views 
 from utilities.rest_api_urls import router 
 register_url = True
 urlpatterns = [
