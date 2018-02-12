f705a59da991f645ea1563f9b4d7fbc3f4984296
xD <cocoaremix@gmail.com>
2018-02-11 16:21:22 -0800

    updating all orders items shipping

--- website/src/orders/urls.py
+++ website/src/orders/urls.py
@@ -1,11 +1,12 @@
 from django.conf.urls import include, url
-from taxes import rest_api as views 
+from rest_api import rest_api as views 
 from utilities.rest_api_urls import router 
 register_url = True
 urlpatterns = [
-	url(r'^taxes/{0,1}$', views.taxes_list),
-	url(r'^tax/{0,1}$', views.tax),
-	url(r'^tax/details/{0,1}$', views.taxes_details),
+	url(r'^orders/{0,1}$', views.order_list),
+	url(r'^order/{0,1}$', views.order),
+	url(r'^order/shipping/list/{0,1}$', views.order_shipping_item_list),
+	url(r'^order/shipping/{0,1}$', views.order_shipping_item),	
 ]
 
 
