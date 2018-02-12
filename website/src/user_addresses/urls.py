77b596b2781b3f61ae15b1d94a4e62c5ea6a25e4
xD <cocoaremix@gmail.com>
2018-02-11 16:24:01 -0800

    updating all orders items shipping

--- website/src/user_addresses/urls.py
+++ website/src/user_addresses/urls.py
@@ -1,12 +1,12 @@
 from django.conf.urls import include, url
 from taxes import rest_api as views 
-from utilities.rest_api_urls import router 
+from user_addresses.rest_api_urls import router 
 register_url = True
 urlpatterns = [
 
-	url(r'^taxes/{0,1}$', views.address_list),
-	url(r'^tax/{0,1}$', views.address),
-	url(r'^tax/details/{0,1}$', views.address_details),
+	url(r'^address/list/{0,1}$', views.address_list),
+	url(r'^address/{0,1}$', views.address),
+	url(r'^address/details/{0,1}$', views.address_details),
 ]
 
 
