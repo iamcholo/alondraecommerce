3f7f4ae14f2f8bbdacf897405639863594f879bd
xD <cocoaremix@gmail.com>
2018-02-13 15:50:44 -0800

    updating card system

--- alondra/src/payments/modules/mercado_pago_paypal/urls.py
+++ alondra/src/payments/modules/mercado_pago_paypal/urls.py
@@ -1,5 +1,5 @@
 from django.conf.urls import include, url
-from payments import rest_api as views 
+from payments.modules.mercado_pago_paypal import rest_api as views 
 register_url = True
 urlpatterns = [
  	url(r'^payment/mercadopago/checkout/{0,1}$', views.create_mercadopago_checkout),
