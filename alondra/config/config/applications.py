690a4daad4da33d3ff5f3fcee3e5638a278bb78b
xD <cocoaremix@gmail.com>
2018-02-14 06:00:14 -0800

    adding social auth

--- alondra/config/config/applications.py
+++ alondra/config/config/applications.py
@@ -8,6 +8,7 @@ INSTALLED_APPS = (
     'django.contrib.staticfiles',
     'django.contrib.humanize',
     'corsheaders',
+    'social_auth',
     'registration',
     'django_countries',
     'rest_framework',
